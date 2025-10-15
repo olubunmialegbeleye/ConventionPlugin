e60e00aba73bdd50fe8e6c6ff5b35fcbaca802e1
olubunmialegbeleye <alegbeleyebunmi@gmail.com>
2025-10-15 16:27:46 +0100

    Step 4.1: Configure convention module
    Configures the `build-logic/convention` Gradle module.
    
    This commit sets up the necessary build configurations, enabling it to define convention plugins for the project.
    
    Changes include:
    - Adding `google()` and `mavenCentral()` repositories.
    - Setting Java source and target compatibility to version 17.
    - Configuring the Kotlin compiler for JVM 17.
    - Adding `compileOnly` dependencies for the Android Gradle Plugin (AGP) and its related tools.

--- build-logic/convention/build.gradle.kts
+++ build-logic/convention/build.gradle.kts
@@ -1,3 +1,28 @@
+import org.jetbrains.kotlin.gradle.dsl.JvmTarget
+
 plugins {
     `kotlin-dsl`
+}
+
+repositories {
+    google()
+    mavenCentral()
+}
+
+java {
+    sourceCompatibility = JavaVersion.VERSION_17
+    targetCompatibility = JavaVersion.VERSION_17
+}
+
+kotlin {
+    compilerOptions {
+        jvmTarget = JvmTarget.JVM_17
+    }
+}
+
+
+dependencies {
+    compileOnly(libs.android.gradleApiPlugin)
+    compileOnly(libs.android.gradlePlugin)
+    compileOnly(libs.android.tools.common)
 }
